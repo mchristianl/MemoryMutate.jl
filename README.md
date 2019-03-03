8597d8b5ff40490915b1c49c5d485dbce30852ab
Christian Lehmann <dev@christianl.de>
2019-03-03 14:16:10 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,169 @@
+
+## MemoryMutate
+
+A macro `@mutate` is provided for multi-_level_ assignments `a.b.c.d.e = v` that uses `fieldoffset` to calculate the offset of the to-be-assigned field relative to the rightmost mutable _level_ and uses `pointer_from_objref` on that rightmost mutable _level_, adds the offset to that pointer and uses `unsafe_store!` to write the value. This compiles down to a few `mov` assembly instructions.
+
+This is a different approach then replacing the whole immutable with a new, modified one
+
+1. Julep: setfield! for mutable references to immutables
+  https://github.com/JuliaLang/julia/issues/17115
+  - "we propose making it possible to have setfield! modify fields inside of immutable objects that are wrapped in mutable objects"
+  - "To support this proposal, the setfield! function will get a multi-arg form, with the following behaviors:"
+    "setfield!(x, a, b, c, value) mutates the right most mutable object to change the value of its fields to be"
+    "equivalent to copying the immutable objects and updating the referenced field."
+  - "tl;dr The syntax:"
+    "x.a.b.c = 3"
+    "would now be valid, as long as at least one of the referenced fields is mutable."
+2. WIP: Make mutating immutables easier
+  https://github.com/JuliaLang/julia/pull/21912
+  - proposes an @ operator as in `x@a = 2` for an immutable `x`
+  - "The ways this works is that under the hood, it creates a new immutable object with the specified field modified and then assigns it back to the appropriate place."
+    "Syntax wise, everything to the left of the @ is what's being assigned to, everything to the right of the @ is what is to be modified."
+
+There are two important justifications to make on which this approach relies:
+1. That immutables, allocated with `Ref` must not be assumed to stay constant when using `unsafe_store!`.
+2. That the memory layout of bitstypes is exaclty what we achieve with `fieldoffset`.
+
+Furthermore, although the assignment becomes just a few `mov` assembly instructions, it is not quite clear which optimizations might break by using `unsafe_store!`.
+
+### Use case
+
+In order to wrap C/C++ libraries in Julia, it is necessary to represent the C-structs as Julia types of the same layout.
+That can be achieved for nested C-structs only with nested immutable Julia types (bitstypes).
+More precisely, the most top level Julia type can be a mutable one, but all contained Julia types need to be immutable (bitstypes).
+
+The [Julia documentation](https://docs.julialang.org/en/v1/manual/calling-c-and-fortran-code/#Passing-Pointers-for-Modifying-Inputs-1) writes
+
+> Because C doesn't support multiple return values, often C functions will take pointers to data that the function will modify.
+
+Consider [the following C/C++ function](https://github.com/ValveSoftware/openvr/blob/master/headers/openvr.h#L3625)
+
+```C++
+/** Computes the overlay-space pixel coordinates of where the ray intersects the overlay with the
+* specified settings. Returns false if there is no intersection. */
+bool ComputeOverlayIntersection(
+	uint64_t ulOverlayHandle,
+	const VROverlayIntersectionParams_t *pParams,
+	VROverlayIntersectionResults_t *pResults
+);
+```
+
+Here `pResults` should hold a pointer to a `VROverlayIntersectionResults_t` C-struct that is defined in the following way
+
+```C++
+enum ETrackingUniverseOrigin
+{
+	…
+};
+
+struct HmdVector3_t
+{
+	float v[3];
+};
+
+struct HmdVector2_t
+{
+	float v[2];
+};
+
+struct VROverlayIntersectionParams_t
+{
+	HmdVector3_t vSource;
+	HmdVector3_t vDirection;
+	ETrackingUniverseOrigin eOrigin;
+};
+
+struct VROverlayIntersectionResults_t
+{
+	HmdVector3_t vPoint;
+	HmdVector3_t vNormal;
+	HmdVector2_t vUVs;
+	float fDistance;
+};
+```
+
+In order to provide C-layout compatible Julia structs, we might use
+
+```julia
+const ETrackingUniverseOrigin = UInt32 # sizeof(ETrackingUniverseOrigin) == 4
+
+struct HmdVector3_t # sizeof(HmdVector3_t) == 12
+  v :: SArray{Tuple{3},Float32,1,3}
+end
+
+struct HmdVector2_t # sizeof(HmdVector2_t) == 8
+  v :: SArray{Tuple{2},Float32,1,2}
+end
+
+struct VROverlayIntersectionParams_t # sizeof(VROverlayIntersectionParams_t) == 28
+  vSource :: HmdVector3_t
+  vDirection :: HmdVector3_t
+  eOrigin :: ETrackingUniverseOrigin
+end
+
+struct VROverlayIntersectionResults_t # sizeof(VROverlayIntersectionResults_t) == 36
+  vPoint :: HmdVector3_t
+  vNormal :: HmdVector3_t
+  vUVs :: HmdVector2_t
+  fDistance :: Float32
+end
+```
+
+A `Ref{VROverlayIntersectionResults_t}` will be passed to Julia's `ccall` and the C/C++ function will alter the underlying immutable - it will mutate the immutable in an arbitrary C-style way.
+
+Therefore we assume that immutables `x`, allocated on the heap via `Ref(x)` are not assumed to be constant anymore by Julia and one can modify them in a C-style way, i.e. modify any parts of the immutables memory.
+
+### What this module provides
+
+Consider the Julia immutable types `Pconst` and `Cconst`
+
+```julia
+struct Pconst # sizeof(Pconst) == 8
+  x :: Float32
+  y :: Float32
+end
+
+struct Cconst # sizeof(Cconst) == 12
+  r :: Float32
+  g :: Float32
+  b :: Float32
+end
+```
+
+and the mutable type `Vmutbl`
+
+```julia
+mutable struct Vmutbl # sizeof(Vmutbl) == 20
+  p :: Pconst
+  c :: Cconst
+end
+```
+
+for
+
+```julia
+p = Pconst(1f0,2f0)     # Pconst(1.0f0, 2.0f0)
+c = Cconst(0f0,1f0,0f0) # Cconst(0.0f0, 1.0f0, 0.0f0)
+v = Vmutbl(p,c)         # Vmutbl(Pconst(1.0f0, 2.0f0), Cconst(0.0f0, 1.0f0, 0.0f0))
+```
+
+we can alter the top-level fields of `v`
+
+```julia
+v.p = Pconst(3f0,2f0)
+v # Vmutbl(Pconst(3.0f0, 2.0f0), Cconst(0.0f0, 1.0f0, 0.0f0))
+```
+
+but not the more nested ones, because they are immutable
+
+```julia
+v.p.x = 5f0
+# ERROR: setfield! immutable struct of type Pconst cannot be change
+```
+
+since we assume that these immutables do not need to stay _constant_, we can replace them at their memory location
+
+```julia
+@mutate v.p.x = 5f0
+v # Vmutbl(Pconst(5.0f0, 3.0f0), Cconst(0.0f0, 1.0f0, 0.0f0))
+```
---
+++ src/MemoryMutate.jl
@@ -0,0 +1,235 @@
+module MemoryMutate
+  export @mutate
+
+  # TODO: may fuse fieldindex_generated, fieldisbitstype_generated and fieldisimmutable_generated into a single function
+
+  # statically unroll the cases for all fieldnames of T
+  @generated function (fieldindex_generated(v::T, f::Symbol)::UInt64) where T
+    # inside the generated function macro, parameters and their type are the same, e.g. v == T
+    sym_idx = gensym()
+    exprs = [:( $sym_idx = UInt64(0) )]
+    for (n,fA) in enumerate(fieldnames(T))
+      push!(exprs, :( f == $(QuoteNode(fA)) && ($sym_idx = UInt64($n)) ))
+    end
+    push!(exprs, :( return $sym_idx ))
+    return Expr(:block,exprs...)
+  end # generates a Core.Compiler.Const((:x, :b), false) and @code_warntype is okay with that
+
+  # statically unroll the cases for all fieldnames of T
+  @generated function fieldisbitstype_generated(::T,f::Symbol) where T
+    # inside the generated function macro, parameters and their type are the same, e.g. v == T
+    sym_bit = gensym()
+    exprs = [:( $sym_bit = false )]
+    for (n,fA) in enumerate(fieldnames(T))
+      push!(exprs, :( f == $(QuoteNode(fA)) && ($sym_bit = isbitstype($(fieldtypes(T)[n]))) ))
+    end
+    push!(exprs, :( return $sym_bit ))
+    return Expr(:block,exprs...)
+  end
+
+  # statically unroll the cases for all fieldnames of T
+  @generated function fieldisimmutable_generated(::T,f::Symbol) where T
+    # inside the generated function macro, parameters and their type are the same, e.g. v == T
+    sym_imm = gensym()
+    exprs = [:( $sym_imm = false )]
+    for (n,fA) in enumerate(fieldnames(T))
+      push!(exprs, :( f == $(QuoteNode(fA)) && ($sym_imm = $(!fieldtypes(T)[n].mutable)) ))
+    end
+    push!(exprs, :( return $sym_imm ))
+    return Expr(:block,exprs...)
+  end
+
+  fieldisbitstype_static(::T, f::Symbol) where T = isbitstype(fieldtype(T,f))
+  refisbitstype_static(::Ref{T}) where T = isbitstype(T)
+  ismutable_static(::T) where T = T.mutable
+  isreference_static(::T) where T = T <: Ref
+  fieldoffset_static(::T, i::UInt64) where T = fieldoffset(T,i)
+
+  # unroll the cases for all fieldnames of T
+  @generated function unsafe_store_generated(::T,f::Symbol,base,off::UInt64,rhs) where T
+    exprs = []
+    for (n,fA) in enumerate(fieldnames(T))
+      push!(exprs,
+        :( f == $(QuoteNode(fA))
+        && ( @assert $(isbitstype(fieldtypes(T)[n])) """
+              From type '$($T)', the field '$($(QuoteNode(fA)))' to be set is itself of type '$($(fieldtypes(T)[n]))', which is not bitstype.
+              This means that the value of field '$($(QuoteNode(fA)))' is an immutable reference
+              and it is opaque to us (is it?) how that is represented in memory (a pointer, for sure) and whether other objects rely on this to be constant.
+              If it would be of type 'Ref{$($(fieldtypes(T)[n]))}' we'd  had a chance to replace it in the memory of it's parent '$($T)'.
+              """
+           ; @GC.preserve base unsafe_store!(reinterpret(Ptr{$(fieldtypes(T)[n])},pointer_from_objref(base)+off),rhs)
+           )
+        ))
+    end
+    return Expr(:block,exprs...)
+  end
+  # fallback for assigning references
+  unsafe_store_generated(::T,::Nothing,base,off::UInt64,rhs) where T = (base[] = rhs) # @assert isa(base,Ref), @assert off == 0
+
+  setfield_or_deref(x::T,f::Symbol,rhs) where T = setfield!(x,f,rhs)
+  setfield_or_deref(x::T,::Nothing,rhs) where T = (x[] = rhs)
+  fieldtype_static(::T, f::Symbol) where T = fieldtype(T,f)
+
+  # structinfo(T) = [(fieldoffset(T,i), fieldname(T,i), fieldtype(T,i)) for i = 1:fieldcount(T)];
+  macro mutate(expr)
+    @assert expr.head == :(=) "Expression for mutating must be an assignment (=)."
+    RHS = expr.args[2]
+
+    # level[n] contains an expression for the current level of access, most likely just a symbol but could be an arbitrary expression as well, or `nothing` if the dereference `[]` occured
+    levels = []
+    cur = expr.args[1]
+    while isa(cur,Expr)
+      if cur.head == :(.)
+        pushfirst!(levels,cur.args[2])
+        cur = cur.args[1]
+      elseif cur.head == :call && cur.args[1] == :getfield # getproperty … is what is called by .
+        pushfirst!(levels,cur.args[3])
+        cur = cur.args[2]
+      elseif cur.head == :ref
+        pushfirst!(levels,nothing)
+        cur = cur.args[1]
+      else
+        break
+      end
+    end
+    pushfirst!(levels,cur)
+    length(levels) <= 1 && return esc(expr)
+
+    # for each level, generate symbols to hold
+    sym_val = [] # every level produces the parent-value for the next level
+    # sym_typ = [] # the type of `val`. It turned out, that it's necessary to use generic or generated functions to obtain compiler constants for the type's properties instead of using the type directly. That is why we introduce `fld`, `idx`, `bit`, `ref` and `mut`.
+    sym_ref = [] # a Bool, whether the type of `val` is mutable
+    sym_mut = [] # a Bool, whether the type of `val` is a reference type
+    sym_fld = [] # this is a symbol (the "value" of the next level's "expression", `level[n+1]`), to be used to produce the `val` of the next level or `nothing` to signal to apply dereferencing `[]`
+    sym_idx = [] # if there is a `fld` symbol, then this is it's numerical index fieldnames-table of the type of `val`. `idx` is zero if the symbol does not occur as a field
+    sym_bit = [] # if there is a `fld` symbol, then this is a Bool, whether the type of `val`.`fld` is a bitstype
+    sym_off = [] # if there is a `fld` symbol, then this is a UInt64 which accumulates the offsets of previous levels and "resets" every time we hit a non-bitstype
+    for m = length(levels):-1:1
+      pushfirst!(sym_val,gensym("val$(m)"))
+      # pushfirst!(sym_typ,gensym("typ$(m)"))
+      pushfirst!(sym_fld,gensym("fld$(m)"))
+      pushfirst!(sym_idx,gensym("idx$(m)"))
+      pushfirst!(sym_bit,gensym("bit$(m)"))
+      pushfirst!(sym_ref,gensym("ref$(m)"))
+      pushfirst!(sym_mut,gensym("mut$(m)"))
+      pushfirst!(sym_off,gensym("off$(m)"))
+    end
+
+    # setting up the expressions to hold compiler constants: for the first level
+    expr_val = [:( $(sym_val[1]) = $(levels[1])                       )]
+    expr_mut = [:( $(sym_mut[1]) = $ismutable_static($(sym_val[1]))   )]
+    expr_ref = [:( $(sym_ref[1]) = $isreference_static($(sym_val[1])) )]
+    expr_idx = []
+    expr_bit = []
+    expr_off = []
+    if levels[2] == nothing # the next level is a dereference `[]`
+      expr_fld = [:( $(sym_fld[1]) = nothing                              )]
+      expr_idx = [:( ;                                                    )]
+      expr_bit = [:( $(sym_bit[1]) = $refisbitstype_static($(sym_val[1])) )]
+      expr_off = [:( $(sym_off[1]) = UInt64(0)                            )]
+    else # the next level provides a symbol
+      expr_fld = [:( $(sym_fld[1]) = $(levels[2])                                             )]
+      expr_idx = [:( $(sym_idx[1]) = $fieldindex_generated($(sym_val[1]),$(sym_fld[1]))       )]
+      expr_bit = [:( $(sym_bit[1]) = $fieldisbitstype_generated($(sym_val[1]), $(sym_fld[1])) )]
+      expr_off = [:( $(sym_off[1]) = $fieldoffset_static($(sym_val[1]), $(sym_idx[1]))        )]
+    end
+
+    # setting up the expressions to hold compiler constants: for the following levels
+    for n=2:length(levels)-1 # every level, we "get" a value: either by dereferencing or by accessing a field
+      if levels[n] == nothing # the current level's value is obtained via dereferencing `[]` the previous level's value
+        push!(expr_val,:( $(sym_val[n]) = $(sym_val[n-1])[] )) # do the dereferencing `[]` to obtain a value
+      else # the current level's value is obtained via a symbol from the previous level's value
+        push!(expr_val,:( $(sym_val[n]) = getfield($(sym_val[n-1]), $(sym_fld[n-1])) )) # do so with `getfield`
+      end
+        push!(expr_mut,:( $(sym_mut[n]) = $ismutable_static($(sym_val[n]))     ))
+        push!(expr_ref,:( $(sym_ref[n]) = $isreference_static($(sym_val[n-1])) ))
+      if levels[n+1] == nothing # the next level is a dereference `[]`
+        push!(expr_fld,:( $(sym_fld[n]) = nothing                              ))
+        push!(expr_idx,:( ;                                                    ))
+        push!(expr_bit,:( $(sym_bit[n]) = $refisbitstype_static($(sym_val[n])) ))
+        push!(expr_off,:( $(sym_off[n]) = UInt64(0)                            ))
+      else # the next level provides a symbol
+        push!(expr_fld,:( $(sym_fld[n]) = $(levels[n+1]) ))
+        push!(expr_idx,:( $(sym_idx[n]) = $fieldindex_generated($(sym_val[n]),$(sym_fld[n])) ))
+        push!(expr_bit,:( $(sym_bit[n]) = $fieldisbitstype_generated($(sym_val[n]), $(sym_fld[n])) ))
+        push!(expr_off,:( $(sym_off[n]) = $fieldoffset_static($(sym_val[n]), $(sym_idx[n])) + ($(sym_bit[n-1]) ? $(sym_off[n-1]) : UInt64(0)) ))
+      end
+    end
+
+    # https://github.com/JuliaLang/julia/issues/10578
+    #   "Comments can be passed through using the Expr(:meta, ...) functionality we have now."
+    #   I am using LineNumberNode instead
+    exprs = []
+    for n=1:length(levels)-1
+      level_str = replace(repr(levels[n]),"\n" => "")
+      push!(exprs,LineNumberNode(-1,"level$(n) $(level_str)"))
+      push!(exprs,expr_val[n])
+      push!(exprs,expr_fld[n])
+      push!(exprs,expr_idx[n])
+      # push!(exprs,expr_chk[n])
+      push!(exprs,expr_bit[n])
+      push!(exprs,expr_mut[n])
+      push!(exprs,expr_off[n])
+    end
+
+    # obtain the result
+    push!(exprs,LineNumberNode(-1,"result"))
+    sym_rhs = gensym("RHS")
+    push!(exprs,:( $sym_rhs = $RHS ))
+
+    #=
+    generate nested `if` statements based on the previously obtained compiler constants to catch all the cases
+      If we are at level[n], then
+      - this level's value is obtained from the previous level's value via dereferencing or via a symbol,
+      - whether this value is a bitstype was already detected in the previous level and it is bit[n-1]
+      - and if this level's value is of a bitstype, then we assume it to be inlined into the parent's struct and therefore the parent's value or one it's ancestors have to be used as a base pointer.
+       - … because pointer_from_objref will fail on bitstypes
+      so it becomes
+      - bit[n-1] ⇒ is this level's field a bitstype? Then use the previous expression from level[n-1] instead.
+      - val[n] ⇒ Else, use this as base and perform store operation
+    =#
+
+    # the first level: use the first level's value as the base to assign at the second-to-last accumulated offset the `rhs` of the second-to-last field's type in the second-to-last value
+    expr_str = :(
+        $(LineNumberNode(-1,"@val1+off$(length(levels)-1) <- val$(length(levels)-1)"))
+      ; $unsafe_store_generated($(sym_val[length(levels)-1]),$(sym_fld[length(levels)-1]),$(sym_val[1]),$(sym_off[length(levels)-1]),$sym_rhs)
+      )
+    if length(levels) == 2 # if the first level is the second-to-last level AND it is mutable, we can use the normal assignment
+      expr_rec = :(
+            $(sym_mut[1]) # is the current level's value of mutable?
+          ? $setfield_or_deref($(sym_val[1]),$(sym_fld[1]),$sym_rhs)
+          : $expr_str # use unsafe_store! on pointer_from_objref for the first level
+          )
+    else # directly use unsafe_store! on pointer_from_objref for the first level
+      expr_rec = expr_str
+    end
+
+    # the remaining levels: use the current level's value as the base to assign at the second-to-last accumulated offset the `rhs` of the second-to-last field's type in the second-to-last value
+    for n=2:length(levels)-1
+      expr_str = :(
+          $(LineNumberNode(-1,"@val$(n)+off$(length(levels)-1) <- val$(length(levels)-1)"))
+        ; $unsafe_store_generated($(sym_val[length(levels)-1]), $(sym_fld[length(levels)-1]), $(sym_val[n]), $(sym_off[length(levels)-1]),$sym_rhs)
+        )
+      if n == length(levels)-1 # if the current level is the second-to-last level AND it is mutable, we can use the normal assignment
+        expr_rec = :( $(sym_bit[n-1]) # is the current level's value of bitstype?
+          ? $expr_rec # use one of the previous level's as a base pointer
+          : ( $(sym_mut[n]) # is the current level's value mutable?
+            ? $setfield_or_deref($(sym_val[length(levels)-1]),$(sym_fld[length(levels)-1]),$sym_rhs)
+            : $expr_str # use unsafe_store! on pointer_from_objref fot the current level
+            )
+          )
+      else
+        expr_rec = :( $(sym_bit[n-1]) # is the current level's value of bitstype?
+          ? $expr_rec # use one of the previous level's as a base pointer
+          : $expr_str # use unsafe_store! on pointer_from_objref fot the current level
+          )
+      end
+    end
+    push!(exprs,:( $expr_rec ))
+
+    # unsafe_store! returns a pointer, but we want the assignment (=) to return the right hand side
+    push!(exprs,:( $sym_rhs ))
+
+    return esc(Expr(:block,exprs...))
+  end
+end
